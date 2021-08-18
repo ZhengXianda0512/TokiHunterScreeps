e5d968327cb159ef5a852e9c90d0df0a3f772983
ZhengXianda <xianda.zheng@jiamiantech.com>
2021-08-18 18:02:24 +0800

    优化全局参数（房间、出生点）获取方式

--- src/GameManager.js
+++ src/GameManager.js
@@ -3,14 +3,14 @@ import { map as SquadMap } from "./squad/map"
 export const GameManager = {
     get room() {
         if (!this._room) {
-            this._room = Game.rooms["sim"]
+            this._room = _.filter(Game.rooms, (room) => true)[0]
         }
         return this._room
     },
 
     get spawn() {
         if (!this._spawn) {
-            this._spawn = Game.spawns["Spawn1"]
+            this._spawn = _.filter(Game.spawns, (spawn) => true)[0]
         }
         return this._spawn
     },
