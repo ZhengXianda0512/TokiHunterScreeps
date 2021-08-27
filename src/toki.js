d0a8cc378e8f41d5519412e77d3b04d0cc371d12
ZhengXianda <xianda.zheng@jiamiantech.com>
2021-08-27 15:44:22 +0800

    添加 UI 展示房间信息

--- src/toki.js
+++ src/toki.js
@@ -16,17 +16,44 @@ global.Toki = {
         return this._spawn
     },
 
+    get controller() {
+        if (!this._controller) {
+            this._controller = this.room.controller
+        }
+        return this._controller
+    },
+
     prepare: function() {
-        let spawning = this.spawn.spawning
+        let room = this.room
+        let spawn = this.spawn
+        let controller = this.controller
+
+        //spawning
+        let spawning = spawn.spawning
         if(spawning) {
             var spawningCreep = Game.creeps[spawning.name]
-            this.spawn.room.visual.text(
+            room.visual.text(
                 '🛠️ ' + spawningCreep.memory.squad,
-                this.spawn.pos.x + 1, 
-                this.spawn.pos.y, 
-                {align: 'left', opacity: 0.8})
+                spawn.pos.x + 1, 
+                spawn.pos.y, 
+                {align: 'left', opacity: 0.8}
+            )
         }
+        spawn.memory.isSpawning = !!spawning
 
-        this.spawn.memory.isSpawning = !!spawning
+        //面板
+        let rowCount = 1
+        for (var i=0; i< rowCount; i++) {
+            room.visual.rect(-0.5,i-0.5,20,1, {stroke: '#000000'})
+        }
+        //控制器数据
+        let increase = controller.progress - room.memory.controller.progress
+        let describe = `Level: ${controller.level} [${controller.progress}(${increase})/${controller.progressTotal}] << ${controller.ticksToDowngrade}`
+        room.visual.text(
+            describe,
+            0,0.2+0,
+            {align: 'left', opacity: 0.8, color: '#000000'}
+        )
+        room.memory.controller = controller
     },
 }
